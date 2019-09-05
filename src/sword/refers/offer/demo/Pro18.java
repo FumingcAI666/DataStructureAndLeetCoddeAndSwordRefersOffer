b7524a46e0ed0c2f099b705ec03cab21aaf397de
付明才 <78781170@qq.com>
2019-09-05 23:43:57 +0800

    剑指offer-18 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。

---
+++ src/sword/refers/offer/demo/Pro18.java
@@ -0,0 +1,19 @@
+package sword.refers.offer.demo;
+
+/**
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
+ * 例如，如果输入如下4 X 4矩阵：
+ * 1 2 3 4
+ * 5 6 7 8
+ * 9 10 11 12
+ * 13 14 15 16 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+ *
+ * @author macfmc
+ * @date 2019/9/4-17:01
+ */
+public class Pro18 {
+    public static void main(String[] args) {
+
+    }
+
+}
