2a2d8fd61db3870199e805f27d99ba3f155b402f
付明才 <78781170@qq.com>
2019-09-08 23:50:53 +0800

    Pro21 从上往下打印出二叉树的每个节点，同层节点从左至右打印。

--- src/sword/refers/offer/demo/Pro21.java
+++ src/sword/refers/offer/demo/Pro21.java
@@ -1,5 +1,7 @@
 package sword.refers.offer.demo;
 
+import java.util.ArrayList;
+
 /**
  * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
  *
@@ -9,5 +11,38 @@ package sword.refers.offer.demo;
 public class Pro21 {
     public static void main(String[] args) {
 
+    }
+
+    /**
+     * 思路是用arraylist模拟一个队列来存储相应的TreeNode
+     */
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<TreeNode> queue = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        queue.add(root);
+        while (queue.size() != 0) {
+            TreeNode temp = queue.remove(0);
+            if (temp.left != null) {
+                queue.add(temp.left);
+            }
+            if (temp.right != null) {
+                queue.add(temp.right);
+            }
+            list.add(temp.val);
+        }
+        return list;
+    }
+
+    class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
     }
 }
