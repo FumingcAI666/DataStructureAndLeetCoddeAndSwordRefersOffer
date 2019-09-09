c7deb76751ada92d19678df3f5777f11fa77fa62
付明才 <78781170@qq.com>
2019-09-09 23:57:16 +0800

    Pro22 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。

---
+++ src/sword/refers/offer/demo/Pro22.java
@@ -0,0 +1,13 @@
+package sword.refers.offer.demo;
+
+/**
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+ *
+ * @author macfmc
+ * @date 2019/9/9-23:55
+ */
+public class Pro22 {
+    public static void main(String[] args) {
+
+    }
+}
