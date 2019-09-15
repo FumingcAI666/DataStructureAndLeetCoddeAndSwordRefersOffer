6c5011062f3ea45490b3994a909e17303b34e634
付明才 <78781170@qq.com>
2019-09-15 22:50:32 +0800

    Pro24 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点），

--- src/sword/refers/offer/demo/Pro24.java
+++ src/sword/refers/offer/demo/Pro24.java
@@ -18,41 +18,39 @@ public class Pro24 {
      * 2、重新遍历链表，复制老结点的随机指针给新结点，如A1.random = A.random.next;
      * 3、拆分链表，将链表拆分为原链表和复制后的链表
      */
-    public class Solution {
-        public RandomListNode Clone(RandomListNode pHead) {
-            if (pHead == null) {
-                return null;
-            }
-
-            RandomListNode currentNode = pHead;
-            //1、复制每个结点，如复制结点A得到A1，将结点A1插到结点A后面；
-            while (currentNode != null) {
-                RandomListNode cloneNode = new RandomListNode(currentNode.label);
-                RandomListNode nextNode = currentNode.next;
-                currentNode.next = cloneNode;
-                cloneNode.next = nextNode;
-                currentNode = nextNode;
-            }
-
-            currentNode = pHead;
-            //2、重新遍历链表，复制老结点的随机指针给新结点，如A1.random = A.random.next;
-            while (currentNode != null) {
-                currentNode.next.random = currentNode.random == null ? null : currentNode.random.next;
-                currentNode = currentNode.next.next;
-            }
-
-            //3、拆分链表，将链表拆分为原链表和复制后的链表
-            currentNode = pHead;
-            RandomListNode pCloneHead = pHead.next;
-            while (currentNode != null) {
-                RandomListNode cloneNode = currentNode.next;
-                currentNode.next = cloneNode.next;
-                cloneNode.next = cloneNode.next == null ? null : cloneNode.next.next;
-                currentNode = currentNode.next;
-            }
-
-            return pCloneHead;
+    public RandomListNode Clone(RandomListNode pHead) {
+        if (pHead == null) {
+            return null;
         }
+
+        RandomListNode currentNode = pHead;
+        //1、复制每个结点，如复制结点A得到A1，将结点A1插到结点A后面；
+        while (currentNode != null) {
+            RandomListNode cloneNode = new RandomListNode(currentNode.label);
+            RandomListNode nextNode = currentNode.next;
+            currentNode.next = cloneNode;
+            cloneNode.next = nextNode;
+            currentNode = nextNode;
+        }
+
+        currentNode = pHead;
+        //2、重新遍历链表，复制老结点的随机指针给新结点，如A1.random = A.random.next;
+        while (currentNode != null) {
+            currentNode.next.random = currentNode.random == null ? null : currentNode.random.next;
+            currentNode = currentNode.next.next;
+        }
+
+        //3、拆分链表，将链表拆分为原链表和复制后的链表
+        currentNode = pHead;
+        RandomListNode pCloneHead = pHead.next;
+        while (currentNode != null) {
+            RandomListNode cloneNode = currentNode.next;
+            currentNode.next = cloneNode.next;
+            cloneNode.next = cloneNode.next == null ? null : cloneNode.next.next;
+            currentNode = currentNode.next;
+        }
+
+        return pCloneHead;
     }
 
     class RandomListNode {
@@ -65,4 +63,6 @@ public class Pro24 {
         }
     }
 }
+
+
 
