c78fc01ba8f318711a582faf33d95905aa659c1a
付明才 <78781170@qq.com>
2019-09-17 22:51:00 +0800

    Pro27 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。

---
+++ src/sword/refers/offer/demo/Pro27.java
@@ -0,0 +1,37 @@
+package sword.refers.offer.demo;
+
+import java.util.Arrays;
+
+/**
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+ * 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+ *
+ * @author macfmc
+ * @date 2019/9/17-22:43
+ */
+public class Pro27 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * @param array
+     * @return
+     */
+    public int MoreThanHalfNum_Solution(int[] array) {
+        Arrays.sort(array);
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == array[array.length / 2]) {
+                count++;
+            }
+        }
+        if (count > array.length / 2) {
+            return array[array.length / 2];
+        } else {
+            return 0;
+        }
+
+    }
+}
