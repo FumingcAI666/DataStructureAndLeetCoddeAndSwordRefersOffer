38dcd66b01b31a1f7490c001f8e977c2642388b3
JackLinkai <2716228597@qq.com>
2020-04-30 00:18:25 +0800

    push 1 283 1013 from jack

--- lk/src/main/java/leetcode/Solution1013.java
+++ lk/src/main/java/leetcode/Solution1013.java
@@ -1,8 +1,12 @@
 package leetcode;
 
 /**将数组分成和相等的三个部分
- * 给你一个整数数组 A，只有可以将其划分为三个和相等的非空部分时才返回 true，否则返回 false。
- * 形式上，如果可以找出索引 i+1 < j 且满足 (A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1]) 就可以将数组三等分。
+ * 给你一个整数数组A
+ * 只有可以将其划分为三个和相等的非空部分时才返回true
+ * 否则返回 false
+ * 形式上，如果可以找出索引i+1<j
+ * 且满足(A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1])
+ * 就可以将数组三等分
  *示例 1：
  * 输入：[0,2,1,-6,6,-7,9,1,2,0,1]
  * 输出：true
@@ -18,7 +22,7 @@ package leetcode;
  * 3 <= A.length <= 50000
  * -10^4 <= A[i] <= 10^4
  *
- * 来源：力扣（LeetCode）
+ *  来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
