4a3e17f0b6b46ca200d9b9d67a68e44bc253c650
YXH-Strive <2833825815@qq.com>
2020-05-01 08:54:31 +0800

    格式化leetcode 1 283 1013

--- src/leetcode/simple283.java
+++ src/leetcode/simple283.java
@@ -1,26 +1,27 @@
 package leetcode;
 
-/**给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
-
- 示例:
- 输入: [0,1,0,3,12]
- 输出: [1,3,12,0,0]
- 说明:
- 必须在原数组上操作，不能拷贝额外的数组。
- 尽量减少操作次数。
-
- 来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/move-zeroes
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author YXH
  * @date 2020/4/29 - 20:13
  */
 public class simple283 {
     public static void main(String[] args) {
-        int[] test1={0,1,0,3,12};
-        int[] test2={0,1,0,3,12,0};
-        int[] test3={5,0,1,0,3,12};
+        int[] test1 = {0, 1, 0, 3, 12};
+        int[] test2 = {0, 1, 0, 3, 12, 0};
+        int[] test3 = {5, 0, 1, 0, 3, 12};
         print(moveZeroes(test1));
         System.out.println();
         print(moveZeroes(test2));
@@ -38,27 +39,27 @@ public class simple283 {
      * @param nums
      */
     public static int[] moveZeroes(int[] nums) {
-        if(nums==null) {
+        if (nums == null) {
             return null;
         }
         //第一次遍历的时候，j指针记录非0的个数，只要是非0的统统都赋给nums[j]
         int j = 0;
-        for(int i=0;i<nums.length;i++) {
-            if(nums[i]!=0) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
                 nums[j++] = nums[i];
             }
         }
         //非0元素统计完了，剩下的都是0了
         //所以第二次遍历把末尾的元素都赋为0即可
-        for(int i=j;i<nums.length;++i) {
+        for (int i = j; i < nums.length; ++i) {
             nums[i] = 0;
         }
         return nums;
     }
 
-    private static void print(int[] nums ){
-        for (int i:nums
-             ) {
+    private static void print(int[] nums) {
+        for (int i : nums
+        ) {
             System.out.print(i);
             System.out.print(" ");
         }
